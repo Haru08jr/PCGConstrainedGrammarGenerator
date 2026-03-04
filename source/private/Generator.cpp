0a8cac0840cf4498825701af773d6a8230f29a15
Haru08jr <jreutercn@gmail.com>
2026-03-04 12:37:48 +0100

    Actually fixed endless loops (hopefully)

--- source/private/Generator.cpp
+++ source/private/Generator.cpp
@@ -4,91 +4,85 @@
 #include <queue>
 
 GenerationResult Generator::generate(const std::map<std::string, GrammarModule>& modules, float maxLength, const NFA& nfa, std::vector<GenerationConstraint> constraints) {
-    std::vector<GenerationResult> correctResults;
-    const float smallestModuleSize = getSmallestModuleSize(modules);
-
     // sort the constraints along the generation shape
     std::sort(constraints.begin(), constraints.end());
 
+    // queue of intermediate results sorted by least amount of epsilon-transitions & shortest length
     std::priority_queue<GenerationResult> queue;
-    queue.emplace(nfa.getStart());
+    // Fill the queue with all outgoing transitions of the start state
+    const GenerationResult start(nfa.getStart());
+    for (const auto& transition : nfa.getAllTransitions(nfa.getStart())) {
+        applyTransitionAndAddToQueue(queue, start, transition, modules, constraints);
+    }
+
+    // saves the most optimal (longest) correct result
+    GenerationResult bestResult(-1);
 
     while (!queue.empty()) {
         const auto currentResult = queue.top();
         queue.pop();
 
-        // for each edge going out of the current state
-        for (const auto& transition: nfa.getAllTransitions(currentResult.currentState)) {
-            GenerationResult newResult = currentResult;
-            newResult.currentState = transition.getTo();
-
-            if (transition.isEpsilon())
-                ++newResult.epsilons;
-            else {
-                auto symbol = transition.getLabel();
-                
-                // abort in case the NFA contains an unknown symbol
-                if (!modules.contains(symbol))
-                {
-                    return GenerationResult(-1);
-                }
-
-                // if at the position of the next relevant constraint
-                if (newResult.constraintsMet < constraints.size() &&
-                    newResult.currentLength <= constraints[newResult.constraintsMet].position &&
-                    constraints[newResult.constraintsMet].position <= newResult.currentLength + modules.at(symbol).size) {
-
-                    // if placing the correct character, mark constraint as solved
-                    if (constraints[newResult.constraintsMet].symbol == symbol) {
-                        ++newResult.constraintsMet;
-                    } else {
-                        // else discard
-                        continue;
-                    }
-                }
-                else if (modules.at(symbol).spawnOnlyWithConstraint)
-                {
-                    // if the symbol should only be placed with a constraint, discard this result
-                    continue;
-                }
-
-                newResult.currentSymbols.push_back(symbol);
-                newResult.currentLength += modules.at(symbol).size;
+        if (nfa.getAccept() == currentResult.currentState && currentResult.constraintsMet == constraints.size()) {
+            // if this result exceeds the maximum length, there will be no better result than the saved one
+            if (currentResult.currentLength > maxLength) {
+                return bestResult;
             }
 
-            // discard result if max length is exceeded
-            if (newResult.currentLength > maxLength) {
-                continue;
+            // if currentResult is longer than the saved best result, save it as the best
+            if (currentResult.currentLength > bestResult.currentLength) {
+                bestResult = currentResult;
             }
-
-            // if at a accepting state & all constraints are met
-            if (nfa.getAccept() == newResult.currentState && newResult.constraintsMet == constraints.size()) {
-                // if you can't add any more symbols without exceeding the maximum length: found optimal result, return
-                if (newResult.currentLength > maxLength - smallestModuleSize) {
-                    return newResult;
-                }
-                // save result as correct
-                correctResults.push_back(newResult);
+        }else {
+            for (const auto& transition: nfa.getAllTransitions(currentResult.currentState)) {
+                applyTransitionAndAddToQueue(queue, currentResult, transition, modules, constraints);
             }
-
-            // else add back to queue
-            queue.emplace(newResult);
         }
     }
 
-    // constraints couldn't be satisfied
-    if (correctResults.empty()) {
-        return GenerationResult(-1);
+    if (!bestResult.isValid())
+        throw GenerationException(GenerationErrorType::ConstraintsNotSatisfiable);
+
+    return bestResult;
+}
+
+void Generator::applyTransitionAndAddToQueue(std::priority_queue<GenerationResult>& queue, const GenerationResult& previousResult, const Edge& transition, const std::map<std::string, GrammarModule>& modules, std::vector<GenerationConstraint> constraints) {
+    GenerationResult newResult = previousResult;
+    newResult.currentState = transition.getTo();
+
+    if (transition.isEpsilon()) {
+        ++newResult.epsilons;
+    }
+    else {
+        const auto symbol = transition.getLabel();
+
+        // abort in case the NFA contains an unknown symbol
+        if (!modules.contains(symbol))
+            throw GenerationException(GenerationErrorType::UnknownLiteral);
+
+        // if at the position of the next relevant constraint
+        if (newResult.constraintsMet < constraints.size() &&
+            newResult.currentLength <= constraints[newResult.constraintsMet].position &&
+            constraints[newResult.constraintsMet].position <= newResult.currentLength + modules.at(symbol).size) {
+
+            // if placing the correct character, mark constraint as solved
+            if (constraints[newResult.constraintsMet].symbol == symbol) {
+                ++newResult.constraintsMet;
+            } else {
+                // else this result is invalid, discard it
+                return;
+            }
+        }
+        else if (modules.at(symbol).spawnOnlyWithConstraint)
+        {
+            // if the symbol should only be placed with a constraint, discard this result
+            return;
+        }
+
+        newResult.currentSymbols.push_back(symbol);
+        newResult.currentLength += modules.at(symbol).size;
     }
 
-    // return the best result found (the longest result with the least amount of epsilons)
-    std::sort(correctResults.begin(), correctResults.end(), [](const GenerationResult& a, const GenerationResult& b) {
-        if (a.currentLength == b.currentLength) {
-            return a.epsilons < b.epsilons;
-        }
-        return a.currentLength > b.currentLength;
-    });
-    return correctResults[0];
+    queue.push(newResult);
 }
 
 float Generator::getSmallestModuleSize(const std::map<std::string, GrammarModule>& modules) {
@@ -96,6 +90,18 @@ float Generator::getSmallestModuleSize(const std::map<std::string, GrammarModule
     for (const auto& [_, module]: modules) {
         if (smallest > module.size)
             smallest = module.size;
+    }
+    return smallest;
+}
+
+float Generator::getSmallestPlaceableModuleSize(const std::map<std::string, GrammarModule>& modules, State state, const NFA& nfa) {
+    float smallest = std::numeric_limits<float>::infinity();
+    for (const auto& edge: nfa.getAllTransitions(state)) {
+        if (!edge.isEpsilon()) {
+            if (modules.contains(edge.getLabel()) && smallest > modules.at(edge.getLabel()).size) {
+                smallest = modules.at(edge.getLabel()).size;
+            }
+        }
     }
     return smallest;
 }
--- source/public/Generator.hpp
+++ source/public/Generator.hpp
@@ -1,6 +1,7 @@
 ﻿#pragma once
 
 #include <map>
+#include <queue>
 #include <vector>
 #include "automaton/Edge.hpp"
 #include "automaton/NFA.hpp"
@@ -41,11 +42,12 @@ struct GenerationResult {
         return  result.substr(0, result.length() - 1);
     }
 
+    /** Shorter results with less epsilons are larger (and therefore prioritized in a queue) */
     bool operator<(const GenerationResult& other) const {
-        if (epsilons == other.epsilons) {
-            return currentLength < other.currentLength;
-        }
-        return epsilons > other.epsilons;
+        if (epsilons != other.epsilons)
+            return epsilons > other.epsilons;
+
+        return currentLength > other.currentLength;
     }
 
     [[nodiscard]] bool isValid() const {
@@ -59,9 +61,18 @@ enum class GenerationErrorType {
     ConstraintsNotSatisfiable
 };
 
+struct GenerationException : std::exception {
+    explicit GenerationException(const GenerationErrorType errorType) : errorType(errorType) {}
+    GenerationErrorType errorType;
+};
+
 class Generator {
 public:
     static GenerationResult generate(const std::map<std::string, GrammarModule>& modules, float maxLength, const NFA& nfa, std::vector<GenerationConstraint> constraints);
 
+    static void applyTransitionAndAddToQueue(std::priority_queue<GenerationResult>& queue, const GenerationResult& previousResult, const Edge& transition, const std::map<std::string, GrammarModule>& modules, std::vector<GenerationConstraint>
+                                             constraints);
+
     static float getSmallestModuleSize(const std::map<std::string, GrammarModule>& modules);
+    static float getSmallestPlaceableModuleSize(const std::map<std::string, GrammarModule>& modules, State state, const NFA& nfa);
 };
