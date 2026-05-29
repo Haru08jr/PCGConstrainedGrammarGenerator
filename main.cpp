589c34f7f723b605cc4f783122194a36da7c9d19
Haru08jr <jreutercn@gmail.com>
2026-05-29 13:42:51 +0200

    Clean up main

--- main.cpp
+++ main.cpp
@@ -13,38 +13,30 @@
 
 int main() {
     std::map<std::string, GrammarModule> modules;
-    modules.emplace("a", GrammarModule{"a", 0.5f});
-    modules.emplace("b", GrammarModule{"b", 0.25f});
+    modules.emplace("a", GrammarModule{"a", 2.f});
+    modules.emplace("b", GrammarModule{"b", 1.f});
 
-    //const auto parseTimeStart = std::chrono::high_resolution_clock::now();
     const RegexParser parser("{a,b}*", {"a", "b"});
     const auto regex = parser.getParsedRegex();
-    //const auto parseTimeEnd = std::chrono::high_resolution_clock::now();
-    //std::cout << (parseTimeEnd - parseTimeStart) << std::endl;
 
-    //const auto compileTimeStart = std::chrono::high_resolution_clock::now();
     const NFACompiler compiler(regex);
     const auto& eNFA = compiler.getConstructedNFA();
-    //const auto compileTimeEnd = std::chrono::high_resolution_clock::now();
-    //std::cout << (compileTimeEnd - compileTimeStart) << std::endl;
 
     //const NFAGraph eGraph(eNFA);
     //eGraph.printGraph("eGraph");
 
     std::vector<GenerationConstraint> constraints;
-    /*
-    constraints.emplace_back("b", 2.5f);
-    constraints.emplace_back("b", 5.f);
-    constraints.emplace_back("b", 7.5f);*/
 
+    constraints.emplace_back("b", 25.f);
+    //constraints.emplace_back("a", 0.f);
 
-    for (int i = 0; i < 10; i++) {
-        const auto generateTimeStart = std::chrono::high_resolution_clock::now();
-        Generator generator(modules, 5.1f, eNFA, constraints);
-        const auto generateTimeEnd = std::chrono::high_resolution_clock::now();
-        std::cout << (generateTimeEnd - generateTimeStart) << std::endl;
-        if (i == 9)
+    //for (int i = 0; i < 10; i++) {
+        //const auto generateTimeStart = std::chrono::high_resolution_clock::now();
+        Generator generator(modules, 25.1f, eNFA, constraints);
+        //const auto generateTimeEnd = std::chrono::high_resolution_clock::now();
+        //std::cout << (generateTimeEnd - generateTimeStart) << std::endl;
+        //if (i == 9)
             std::cout << generator.getGenerationResult().getGeneratedString() << std::endl << std::flush;
-    }
+    //}
     return 0;
 }
